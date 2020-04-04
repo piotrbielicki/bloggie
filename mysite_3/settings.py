ad53474d281aad6df61841cb55856d68983c9532
Piotr Bielicki <pantherbielicki2@vp.pl>
2020-04-05 01:18:28 +0200

    register haystack in installed_apps

--- mysite_3/settings.py
+++ mysite_3/settings.py
@@ -43,7 +43,15 @@ INSTALLED_APPS = (
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'haystack'
 )
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8983/solr/bloggie'
+    },
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
